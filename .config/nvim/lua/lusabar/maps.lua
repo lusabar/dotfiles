959379aa3977a58c4a8531127a1f26ef4e955d55
Lucas Sales Barbosa <lucassalesb10@gmail.com>
2025-04-04 13:18:04 -0400

    Sets keymaps

--- .config/nvim/lua/lusabar/maps.lua
+++ .config/nvim/lua/lusabar/maps.lua
@@ -1,2 +1,20 @@
 -- Sets leader key to space
 vim.g.mapleader = " "
+
+-- Move selected around
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep cursor in place when using J
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Paste over selected without changing current paste buffer
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Yank to clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- Changes current word
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
