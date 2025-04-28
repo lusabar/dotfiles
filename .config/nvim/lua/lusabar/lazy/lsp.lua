ce7580687df518a4a0a715110bb58fccddfb6885
Lucas Sales Barbosa <lucassalesb10@gmail.com>
2025-04-28 18:30:32 -0400

    Adds LSP

---
+++ .config/nvim/lua/lusabar/lazy/lsp.lua
@@ -0,0 +1,25 @@
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "stevearc/conform.nvim",
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "j-hui/fidget.nvim",
+    },
+
+    config = function()
+        require("lspconfig").lua_ls.setup({})
+        require("lspconfig").julials.setup({})
+        require("lspconfig").texlab.setup({})
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+        })
+    end
+}
