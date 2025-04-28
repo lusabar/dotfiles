d28105e2ddd50105be7ee5c913be398867cdff60
Lucas Sales Barbosa <lucassalesb10@gmail.com>
2025-04-28 16:56:26 -0400

    Adds highlight when yanking

--- .config/nvim/lua/lusabar/set.lua
+++ .config/nvim/lua/lusabar/set.lua
@@ -25,3 +25,12 @@ vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 
 vim.g.mapleader = " "
+
+-- Highlight when yanking
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = "Highlight when yanking text",
+    group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+    callback = function ()
+        vim.highlight.on_yank()
+    end,
+})
