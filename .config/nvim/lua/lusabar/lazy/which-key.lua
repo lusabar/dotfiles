a404b58a514e69a5adf54a2fcf9545c1ebe07f2c
Lucas Sales Barbosa <lucassalesb10@gmail.com>
2025-04-29 13:04:22 -0400

    Formatting changes

--- .config/nvim/lua/lusabar/lazy/which-key.lua
+++ .config/nvim/lua/lusabar/lazy/which-key.lua
@@ -1,18 +1,19 @@
+--local wk = require("which-key")
 return {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-    },
-    keys = {
-        {
-            "<leader>?",
-            function()
-                require("which-key").show({ global = false })
-            end,
-            desc = "Buffer Local Keymaps (which-key)",
-        },
-    },
+   "folke/which-key.nvim",
+   event = "VeryLazy",
+   opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+   },
+   keys = {
+      {
+         "<leader>?",
+         function()
+            require("which-key").show({ global = false })
+         end,
+         desc = "Buffer Local Keymaps (which-key)",
+      },
+   }
 }
