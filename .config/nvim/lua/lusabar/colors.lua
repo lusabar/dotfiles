b7310fd394df71c0956b80309bf102325483bcba
Lucas Sales Barbosa <lucassalesb10@gmail.com>
2025-05-24 08:39:02 -0400

    adds color_everything function allows for interactively changing of colorschemes

--- .config/nvim/lua/lusabar/colors.lua
+++ .config/nvim/lua/lusabar/colors.lua
@@ -1,6 +1,11 @@
 -- Select colorscheme
-color = "rose-pine"
-vim.cmd.colorscheme(color)
 
-vim.api.nvim_set_hl(0, "Normal", {bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none" })
+function color_everything(color)
+   color = color or "default"
+   vim.cmd.colorscheme(color)
+
+   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+color_everything("lavender")
